9697bd5d4cb244f6b7ca6feb061535726bcd6438
Felix Toledo <felixtoledoctes@gmail.com>
2025-12-03 08:54:20 -0300

    user service to have default user data

---
+++ apps/back-end/src/users/dto/user-info.response.dto.ts
@@ -0,0 +1,9 @@
+export class UserInfoResponseDTO {
+  id: string;
+  mail: string;
+  username: string | null;
+  avatarUrl: string | null;
+  name: string;
+  lastName: string;
+  isVerified: boolean;
+}
---
+++ apps/back-end/src/users/users.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Get,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../shared/guards/jwt-auth.guard';
+import { UsersService } from './users.service';
+
+@ApiTags('Users')
+@Controller('api/users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  async updateProfile(@Req() req) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const userId: string = req.user.userId as string;
+    return await this.userService.getUserInfo(userId);
+  }
+}
